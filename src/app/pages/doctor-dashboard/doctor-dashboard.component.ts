f6f9cdcea566e80a0c43b53d389850d2cbf33047
Dzevad.Alibegovic <dzevad.alibegovic@edu.fit.ba>
2023-07-17 13:09:00 +0200

    dashboard refactor

--- src/app/pages/doctor-dashboard/doctor-dashboard.component.ts
+++ src/app/pages/doctor-dashboard/doctor-dashboard.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [
     './doctor-dashboard.component.scss',
     '../../../assets/css/authenticated-style.css',
-    '../../../assets/css/font-awesome.min.css'
-
+    '../../../assets/css/font-awesome.min.css',
+    '../../../assets/css/bootstrap.min.css',
     ]
 })
 export class DoctorDashboardComponent implements OnInit {
